8d559f1f839acdedec3ad887970321584c7cc51b
cangokalp <cangokalp@utexas.edu>
2019-12-19 15:31:08 -0600

    a_star search changes

--- a_star_seq.py
+++ a_star_seq.py
@@ -1,5 +1,9 @@
 from test_add_remove import *
 
+SNAME = 'Moderate_5'
+NETFILE = "SiouxFalls/SiouxFalls_net.tntp"
+TRIPFILE = "SiouxFalls/SiouxFalls_trips.tntp"
+
 
 class Node():
     """A node class for A* Pathfinding"""
@@ -12,7 +16,7 @@ class Node():
         self.net = net
         self.tstt_before = tstt_before
         self.tstt_after = tstt_after
-        self.damage_dict = damage_dict
+        self.damaged_dict = damaged_dict
         self.g = 0
         self.h = 0
         self.f = 0
@@ -21,8 +25,8 @@ class Node():
     def assign_char(self):
 
         if self.parent is not None:
-            self.benefit = tstt_after - tstt_before
-            self.days = damage_dict[link_id]
+            self.benefit = self.tstt_after - self.tstt_before
+            self.days = self.damaged_dict[self.link_id]
             prev_path = deepcopy(self.parent.path)
             prev_path.append(self.link_id)
             self.path = prev_path
@@ -45,8 +49,7 @@ class Node():
         return self.visited == other.visited
 
 
-def set_bounds(node, wb, bb):
-    remaining = get_successors(node)
+def set_bounds(node, wb, bb, remaining):
     node.ub = node.realized
     node.lb = node.realized
 
@@ -54,7 +57,7 @@ def set_bounds(node, wb, bb):
     orderedw_benefits = []
     orderedb_benefits = []
 
-    sorted_d = sorted(damage_dict.items(), key=lambda x: x[1])
+    sorted_d = sorted(node.damaged_dict.items(), key=lambda x: x[1])
 
     for key, value in sorted_d:
         # print("%s: %s" % (key, value))
@@ -91,21 +94,24 @@ def set_bounds(node, wb, bb):
         node.ub += w_tstt * days_w[i]
 
 
-def get_successors(node, wb, bb):
+def get_successors(node, wb, bb, to_visit):
     """given a state, returns list of bridges that has not been fixed yet"""
     not_visited = to_visit.difference(node.visited)
     successors = []
 
-    tail = node.path[-1]
-    for a_link in not_visited:
-        if wb[a_link] * damage_dict[tail] - bb[tail] * damage_dict[a_link] > 0:
-            continue
-        successors.append(a_link)
+    if node.level != 0:
+        tail = node.path[-1]
+        for a_link in not_visited:
+            if wb[a_link] * node.damaged_dict[tail] - bb[tail] * node.damaged_dict[a_link] > 0:
+                continue
+            successors.append(a_link)
+    else:
+        successors = not_visited
 
     return not_visited
 
 
-def expand_sequence(node, a_link, level):
+def expand_sequence(node, a_link, level, damaged_dict):
     """given a link and a node, it expands the sequence"""
     tstt_before = node.tstt_after
     net = deepcopy(node.net)
@@ -113,16 +119,15 @@ def expand_sequence(node, a_link, level):
     solve_UE(net=net)
     tstt_after = find_tstt(net=net)
     node = Node(link_id=a_link, parent=node, net=net, tstt_after=tstt_after,
-                tstt_before=tstt_before, level=level, damaged_dict=damage_dict)
-    set_bounds(node, wb, bb)
+                tstt_before=tstt_before, level=level, damaged_dict=damaged_dict)
     return node
 
 
-def astar(damage_dict, wb, bb, start_node, end_node):
+def astar(damaged_dict, wb, bb, start_node, end_node):
     """Returns the best order to visit the set of nodes"""
 
-    damaged_links = damage_dict.keys()
-    repair_days = damage_dict.values()
+    damaged_links = damaged_dict.keys()
+    repair_days = damaged_dict.values()
     to_visit = set(damaged_links)
 
     # Initialize both open and closed list
@@ -131,9 +136,10 @@ def astar(damage_dict, wb, bb, start_node, end_node):
 
     # Add the start node
     open_list.append(start_node)
-
+    best_ub = np.inf
     # Loop until you find the end
-    pdb.set_trace()
+    print('a-star search starting...')
+    tap_solved = 0
     while len(open_list) > 0:
 
         # Get the current node
@@ -146,34 +152,49 @@ def astar(damage_dict, wb, bb, start_node, end_node):
 
         # Pop current off open list, add to closed list
         open_list.pop(current_index)
+        closed_list.append(current_node)
 
         # Found the goal
         if current_node == end_node:
-            return end_node.path
+            return current_node.path
+
+        if current_node.f > best_ub:
+            continue
+
+
 
         # Generate children
-        eligible_expansions = get_successors(current_node, wb, bb)
+        eligible_expansions = get_successors(current_node, wb, bb, to_visit)
+        
         children = []
 
         for a_link in eligible_expansions:
 
             # Create new node
             new_node = expand_sequence(
-                current_node, a_link, level=current_node.level + 1)
-
+                current_node, a_link, level=current_node.level + 1, damaged_dict=damaged_dict)
+            tap_solved += 1
             # Append
             children.append(new_node)
 
+        print('open_list length: ', len(open_list))
+        print('number of taps solved: ', tap_solved)
         # Loop through children
         for child in children:
 
+            remaining = get_successors(child, wb, bb, to_visit)
             # set upper and lower bounds
-            set_bounds(current_node, wb, bb)
+            set_bounds(child, wb, bb, remaining)
+            best_ub = min(best_ub, child.ub)
 
             # Create the f, g, and h values
             child.g = child.realized
-            child.h = child.lb
-            child.f = child.g + child.h
+            # child.h = child.lb - child.g
+            # child.f = child.g + child.h
+            child.f = child.lb
+
+            if child.f > best_ub:
+                continue
 
             # Child is already in the open list
             removal = []
@@ -196,14 +217,13 @@ def astar(damage_dict, wb, bb, start_node, end_node):
             # Add the child to the open list
             open_list.append(child)
 
-        closed_list.append(current_node)
 
 
 def create_network(netfile=None, tripfile=None):
     return Network(netfile, tripfile)
 
 def worst_benefits(before, links_to_remove, before_eq_tstt):
-    if not os.path.exists('saved_dictionaries/' + 'worst_benefit_dict' + sname + '.pickle'):
+    if not os.path.exists('saved_dictionaries/' + 'worst_benefit_dict' + SNAME + '.pickle'):
 
         # for each bridge, find the effect on TSTT when that bridge is removed
         # while keeping others
@@ -214,15 +234,15 @@ def worst_benefits(before, links_to_remove, before_eq_tstt):
             solve_UE(net=test_net)
             wb[link] = find_tstt(test_net) - before_eq_tstt
         # save dictionary
-        save(wb, 'worst_benefit_dict' + sname)
+        save(wb, 'worst_benefit_dict' + SNAME)
 
     else:
-        wb = load('worst_benefit_dict' + sname)
+        wb = load('worst_benefit_dict' + SNAME)
 
     return wb
 
 def best_benefit(after, links_to_remove, after_eq_tstt):
-    if not os.path.exists('saved_dictionaries/' + 'best_benefit_dict' + sname + '.pickle'):
+    if not os.path.exists('saved_dictionaries/' + 'best_benefit_dict' + SNAME + '.pickle'):
 
         # for each bridge, find the effect on TSTT when that bridge is removed
         # while keeping others
@@ -235,51 +255,73 @@ def best_benefit(after, links_to_remove, after_eq_tstt):
             tstt_after = find_tstt(test_net)
 
             seq_list.append(Node(link_id=link, parent=None, net=test_net, tstt_after=tstt_after,
-                                tstt_before=after_eq_tstt, level=1, damaged_dict=damage_dict))
+                                tstt_before=after_eq_tstt, level=1, damaged_dict=damaged_dict))
             bb[link] = after_eq_tstt - tstt_after
-        save(bb, 'best_benefit_dict' + sname)
-        save(seq_list, 'seq_list' + sname)
+        save(bb, 'best_benefit_dict' + SNAME)
+        save(seq_list, 'seq_list' + SNAME)
 
     else:
-        bb = load('best_benefit_dict' + sname)
-        # seq_list = load('seq_list' + sname)
+        bb = load('best_benefit_dict' + SNAME)
+        # seq_list = load('seq_list' + SNAME)
 
     return bb
 
+def state_after(damaged_links):
+    if not os.path.exists('saved_dictionaries/' + 'net_after' + SNAME + '.pickle'):
 
-# def main():
+        net_after = create_network(NETFILE, TRIPFILE)
+        for link in damaged_links:
+            net_after.link[link].remove()
+        solve_UE(net=net_after)
+        after_eq_tstt = find_tstt(net=net_after)
+        
+        save(net_after, 'net_after' + SNAME)
+        save(after_eq_tstt, 'tstt_after' + SNAME)
 
-SNAME = 'Moderate_5'
-NETFILE = "SiouxFalls/SiouxFalls_net.tntp"
-TRIPFILE = "SiouxFalls/SiouxFalls_trips.tntp"
+    else:
+        net_after = load('net_after' + SNAME)
+        after_eq_tstt = load('tstt_after' + SNAME)
 
-damage_dict = read_scenario(sname=sname)
-damaged_links = damaged_dict.keys()
+    return net_after, after_eq_tstt
 
-net_before = create_network(NETFILE, TRIPFILE)
-solve_UE(net=net_before)
-before_eq_tstt = find_tstt(net=net_before)
+def state_before(damaged_links):
+    
+    if not os.path.exists('saved_dictionaries/' + 'net_before' + SNAME + '.pickle'):
+        net_before = create_network(NETFILE, TRIPFILE)
+        solve_UE(net=net_before)
+        before_eq_tstt = find_tstt(net=net_before)
+        
+        save(net_before, 'net_before' + SNAME)
+        save(before_eq_tstt, 'tstt_before' + SNAME)
 
-net_after = create_network(NETFILE, TRIPFILE)
-for link in damaged_links:
-    after.link[link].remove()
-solve_UE(net=net_after)
-after_eq_tstt = find_tstt(net=net_after)
+    else:
+        net_before = load('net_before' + SNAME)
+        before_eq_tstt = load('tstt_before' + SNAME)
 
-wb = worst_benefits(net_before, damaged_links, before_eq_tstt)
-bb = best_benefit(net_after, damaged_links, after_eq_tstt)
+    return net_before, before_eq_tstt
 
-# Create start and end node
-start_node = Node(tstt_after=before_eq_tstt)
-start_node.realized = 0
-start_node.level = 0
-start_node.visited = set([])
+def main():
 
-end_node = Node()
-end_node.level = len(damaged_links) + 1
-end_node.visited = set([damaged_links, 'end'])
+    damaged_dict = read_scenario(sname=SNAME)
+    damaged_links = [link for link in damaged_dict.keys()]
 
-astar(damage_dict, wb, bb, start_node, end_node)
+    net_after, after_eq_tstt = state_after(damaged_links)
+    net_before, before_eq_tstt = state_before(damaged_links)
 
-# if __name__ == '__main__':
-#     main()
+    wb = worst_benefits(net_before, damaged_links, before_eq_tstt)
+    bb = best_benefit(net_after, damaged_links, after_eq_tstt)
+
+    # Create start and end node
+    start_node = Node(tstt_after=before_eq_tstt, net=net_after)
+    start_node.realized = 0
+    start_node.level = 0
+    start_node.visited = set([])
+
+    end_node = Node()
+    end_node.level = len(damaged_links) + 1
+    end_node.visited = set(damaged_links).union(['end'])
+
+    astar(damaged_dict, wb, bb, start_node, end_node)
+
+if __name__ == '__main__':
+    main()
