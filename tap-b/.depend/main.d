df67059b230fc1ff09ec67271634b9c959e05ea4
cangokalp <cangokalp@utexas.edu>
2020-07-07 09:43:59 -0500

    saved experiments

--- tap-b/.depend/main.d
+++ tap-b/.depend/main.d
@@ -1,6 +1,6 @@
 obj/main.o: src/main.c include/main.h include/fileio.h include/bush.h \
-  include/tap.h include/networks.h include/datastructures.h \
-  include/utils.h
+ include/tap.h include/networks.h include/datastructures.h \
+ include/utils.h
 
 include/main.h:
 
